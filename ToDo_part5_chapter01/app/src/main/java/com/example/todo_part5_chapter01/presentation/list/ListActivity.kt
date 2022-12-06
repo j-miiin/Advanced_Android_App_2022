9235e0c0694cba8487fa0420fb643763ff36ee9e
jungmin <kimjm0825@gmail.com>
2022-12-06 18:31:08 +0900

    layout 작성

--- ToDo_part5_chapter01/app/src/main/java/com/example/todo_part5_chapter01/presentation/list/ListActivity.kt
+++ ToDo_part5_chapter01/app/src/main/java/com/example/todo_part5_chapter01/presentation/list/ListActivity.kt
@@ -1,9 +1,11 @@
 package com.example.todo_part5_chapter01.presentation.list
 
 import android.os.Bundle
-import com.example.todo_part5_chapter01.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo_part5_chapter01.databinding.ActivityListBinding
 import com.example.todo_part5_chapter01.presentation.BaseActivity
+import com.example.todo_part5_chapter01.presentation.detail.DetailMode
+import com.example.todo_part5_chapter01.presentation.view.ToDoAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -17,6 +19,8 @@ internal class ListActivity : BaseActivity<ListViewModel>(), CoroutineScope {
 
     private lateinit var binding: ActivityListBinding
 
+    private var adapter = ToDoAdapter()
+
     override val viewModel: ListViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,19 @@ internal class ListActivity : BaseActivity<ListViewModel>(), CoroutineScope {
     }
 
     private fun initViews() = with(binding) {
+        recyclerView.layoutManager = LinearLayoutManager(this@ListActivity, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = adapter
 
+        refreshLayout.setOnRefreshListener {
+            viewModel.fetchData()
+        }
+
+        addToDoButton.setOnClickListener {
+            startActivityForResult(
+                DetailActivity.getIntent(this@ListActivity, DetailMode.WRITE),
+                DetailActivity.FETCH_REQUEST_CODE
+            )
+        }
     }
 
     override fun observeData() {
