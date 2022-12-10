c77ca8bc2459c241e2cfc47ba1fe6b1faa5b01aa
jungmin <kimjm0825@gmail.com>
2022-12-11 02:24:52 +0900

    상세화면 및 주문 기능

---
+++ Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProductDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shopping_app_part5_chapter02.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
---
+++ Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProdutDetailViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.shopping_app_part5_chapter02.presentation.detail
+
+import com.example.shopping_app_part5_chapter02.presentation.BaseViewModel
+import kotlinx.coroutines.Job
+
+internal class ProdutDetailViewModel: BaseViewModel() {
+    override fun fetchData(): Job {
+        TODO("Not yet implemented")
+    }
+}
