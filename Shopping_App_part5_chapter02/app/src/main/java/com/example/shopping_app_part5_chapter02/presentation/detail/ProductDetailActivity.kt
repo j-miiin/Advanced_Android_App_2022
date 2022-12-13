581f56fcd1b54a54882493a6401d2c4062800aff
jungmin <kimjm0825@gmail.com>
2022-12-13 23:53:39 +0900

    상세화면 구성

--- Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProductDetailActivity.kt
+++ Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProductDetailActivity.kt
@@ -2,8 +2,15 @@ package com.example.shopping_app_part5_chapter02.presentation.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shopping_app_part5_chapter02.R
+import com.example.shopping_app_part5_chapter02.databinding.ActivityProductDetailBinding
+import com.example.shopping_app_part5_chapter02.presentation.BaseActivity
+import org.koin.android.ext.android.inject
 
-class ProductDetailActivity : AppCompatActivity() {
+internal class ProductDetailActivity : BaseActivity<ProductDetailViewModel, ActivityProductDetailBinding>() {
+
+    override val viewModel  by inject<ProductDetailViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
--- Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProductDetailViewModel.kt
+++ Shopping_App_part5_chapter02/app/src/main/java/com/example/shopping_app_part5_chapter02/presentation/detail/ProductDetailViewModel.kt
@@ -3,7 +3,7 @@ package com.example.shopping_app_part5_chapter02.presentation.detail
 import com.example.shopping_app_part5_chapter02.presentation.BaseViewModel
 import kotlinx.coroutines.Job
 
-internal class ProdutDetailViewModel: BaseViewModel() {
+internal class ProductDetailViewModel: BaseViewModel() {
     override fun fetchData(): Job {
         TODO("Not yet implemented")
     }
