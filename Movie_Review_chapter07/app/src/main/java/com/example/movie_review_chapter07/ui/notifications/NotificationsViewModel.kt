c82c3fc295e1f36463734ac6af9a18e912f88161
jungmin <kimjm0825@gmail.com>
2023-01-10 21:11:49 +0900

    영화 평점 서비스 프로젝트 셋업

---
+++ Movie_Review_chapter07/app/src/main/java/com/example/movie_review_chapter07/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.movie_review_chapter07.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
